46dfd923c6716aba07ee368e6b2ff2ce7f2978b8
DenisChertanov <dachertanov@edu.hse.ru>
2022-03-17 00:11:28 +0300

    Добавил рандомность значений

--- ReceiverService/src/main/java/ru/analytical/storage/receiver/controller/UserEventController.java
+++ ReceiverService/src/main/java/ru/analytical/storage/receiver/controller/UserEventController.java
@@ -2,7 +2,7 @@ package ru.analytical.storage.receiver.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,17 +11,35 @@ import org.springframework.web.bind.annotation.RestController;
 import ru.analytical.storage.receiver.dto.UserEvent;
 import ru.analytical.storage.receiver.kafka.producer.UserEventsProducer;
 
+import java.util.Random;
+
+@Slf4j
 @Tag(name = "Активность пользователя", description = "Сохраняет историю активности пользователя")
 @RestController
 @RequestMapping("/api/user-event")
-@AllArgsConstructor
 public class UserEventController {
     private final UserEventsProducer userEventsProducer;
+    private final Random rand;
+
+    public UserEventController(UserEventsProducer userEventsProducer) {
+        this.userEventsProducer = userEventsProducer;
+        this.rand = new Random();
+    }
 
     @PostMapping("/save")
     @Operation(summary = "Сохраняет событие пользователя")
     public ResponseEntity<String> saveUserEvent(@RequestBody UserEvent userEvent) {
+        changeUserEventToRandomValue(userEvent);
+        log.info("Random UserEvent: {}", userEvent);
+
         userEventsProducer.sendUserEvent(String.valueOf(userEvent.getUserId()), userEvent);
         return ResponseEntity.ok().build();
+    }
+
+    private void changeUserEventToRandomValue(UserEvent userEvent) {
+        userEvent.setUserId((long) rand.nextInt(100000));
+        userEvent.setVideoId((long) rand.nextInt(100000));
+        userEvent.setEventType((long) rand.nextInt(100000));
+        userEvent.setEventTime((long) rand.nextInt(100000));
     }
 }
