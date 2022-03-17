d23632cde85e4461163469304cb1e6fd4c176984
Николай Сафонов <uporoty-cot@mail.ru>
2022-03-17 15:50:06 +0300

    add dockerfile for cassandra http adapter

--- tank/cassandra/http-adapter/src/main/java/http_adapter/DataGeneratorApplication.java
+++ tank/cassandra/http-adapter/src/main/java/http_adapter/DataGeneratorApplication.java
@@ -1,10 +1,9 @@
-package data_generator;
+package http_adapter;
 
 import com.datastax.oss.driver.api.core.CqlSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
